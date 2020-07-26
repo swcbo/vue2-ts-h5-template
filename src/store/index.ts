3ad6528d3005143c06fabe0789a98018fea37f84
黄波 <1104133609@qq.com>
2020-07-26 13:25:29 +0800

    vuex案例

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,18 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+/*
+ * @Description: store
+ * @Author: 小白
+ * @Date: 2019-11-22 09:24:57
+ * @LastEditors: 小白
+ * @LastEditTime: 2020-03-17 09:34:24
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { IUserState } from './modules/user'
+Vue.use(Vuex)
 
-Vue.use(Vuex);
+export interface IRootState {
+    user: IUserState
+}
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
-});
+// Declare empty store first, dynamically register all modules later.
+export default new Vuex.Store<IRootState>({})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,28 +4,35 @@
  * @Author: 小白
  * @Date: 2020-07-25 08:06:14
  * @LastEditors: 小白
- * @LastEditTime: 2020-07-26 11:39:00
+ * @LastEditTime: 2020-07-26 13:25:04
 --> 
 <!--  -->
 <template>
   <div>
     32131212312
-    <Button class="mT-20">{{title}}</Button>
+    <Button
+      class="mT-20"
+      @click="OnTitle"
+    >{{token}}</Button>
   </div>
 </template>
 
 <script lang='ts'>
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { Button } from 'vant';
+import { UserModule } from '../store/modules/user';
 @Component({ components: { Button }, name: '' })
 export default class extends Vue {
-  private title = '321312321';
+  get token() {
+    return UserModule.token;
+  }
+  private OnTitle() {
+    UserModule.SET_TOKEN('测试修改token');
+  }
   private created() {
     console.log(process.env.VUE_APP_BASE_API);
   }
 }
 </script>
 <style lang='scss' scoped>
-.mT-20{
-}
 </style>
