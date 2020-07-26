828bf785d8116869bb92e9429ff9ff0e8f1e7854
黄波 <1104133609@qq.com>
2020-07-26 13:39:59 +0800

    添加fastclick

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@
  * @Author: 小白
  * @Date: 2020-07-25 08:06:14
  * @LastEditors: 小白
- * @LastEditTime: 2020-07-26 11:37:10
+ * @LastEditTime: 2020-07-26 13:39:50
  */ 
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './assets/css/common.scss'
+import {FastClick} from 'fastclick'
+FastClick.attach(document.body)
 Vue.config.productionTip = false;
 new Vue({
 	router,
