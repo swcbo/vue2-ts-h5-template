45c89a2a8c4dce2ec41ac77c3cd9f9115daa6518
huangbo <1104133609@qq.com>
2020-08-15 13:56:35 +0800

    兼容ie

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@
  * @Author: 小白
  * @Date: 2020-07-25 08:06:14
  * @LastEditors: 小白
- * @LastEditTime: 2020-08-13 09:33:36
+ * @LastEditTime: 2020-08-15 13:56:21
  */
-
+import 'babel-polyfill';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
